42a414d2215722936a633473b17231cc96171083
Sahil Ahuja <sahilahuja@gmail.com>
2017-02-13 10:36:54 +0530

    Fixing file permissions

--- docker_related/setupdb.sh
+++ docker_related/setupdb.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd /src
-php artisan migrate -n --force
-php artisan db:seed -n --force
+sudo -u www-data php artisan migrate -n --force
+sudo -u www-data php artisan db:seed -n --force
 chown -R www-data /src/storage
