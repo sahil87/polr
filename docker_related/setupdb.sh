f3aeadbc3b535c5a21fb05bff54108ec6ea2889e
Sahil Ahuja <sahilahuja@gmail.com>
2017-02-28 11:56:27 +0530

    Making sure storage folder exists

--- docker_related/setupdb.sh
+++ docker_related/setupdb.sh
@@ -2,4 +2,5 @@
 cd /src
 sudo -u www-data php artisan migrate -n --force
 sudo -u www-data php artisan db:seed -n --force
-chown -R www-data /src/storage
+#storage folder assumed to exist in polr code
+sudo mkdir -p /src/storage; chown -R www-data /src/storage
