a115073bd8e4f267b7e1352dc8def2b372a55764
Sahil Ahuja <sahilahuja@gmail.com>
2017-02-28 12:47:34 +0530

    Adding API to seeder

--- docker_related/setupdb.sh
+++ docker_related/setupdb.sh
@@ -2,5 +2,4 @@
 cd /src
 sudo -u www-data php artisan migrate -n --force
 sudo -u www-data php artisan db:seed -n --force
-#storage folder assumed to exist in polr code
-sudo mkdir -p /src/storage; chown -R www-data /src/storage
+chown -R www-data /src/storage
