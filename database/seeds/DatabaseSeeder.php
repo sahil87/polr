b199951ae2b9e09ca62c912624fccd09d7eca093
Sahil Ahuja <sahilahuja@gmail.com>
2019-02-23 23:10:17 +0530

    Squashig changes

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,8 +14,25 @@ class DatabaseSeeder extends Seeder
     {
         Model::unguard();
 
-        // $this->call('UserTableSeeder');
+        $this->call('UserTableSeeder');
 
         Model::reguard();
     }
+}
+
+class UserTableSeeder extends Seeder {
+
+    public function run()
+    {
+        //DB::table('users')->delete();
+        DB::table('users')->insert([
+            'username' => 'polr',
+            'email' => 'polr@admin.tld',
+            'password' => bcrypt('polr'),
+            'role' => 'admin',
+            'active' => '1',
+            'api_key' => '7789835af06daaf863bcdce26e75fe'
+        ]);
+    }
+
 }
